30983311d70f24cf07dc4bcf04a01be972bb3cc1
mariyadas208 <b.mariyadas666@gmail.com>
2023-04-21 12:52:47 +0530

    changes in sysout

--- src/com/example2/example2.java
+++ src/com/example2/example2.java
@@ -3,7 +3,7 @@ package com.example2;
 public class example2 {
 
 	public static void main(String[] args) {
-		System.out.println("das good morning");
+		System.out.println("das good morning how are ");
 	}
 
 }
